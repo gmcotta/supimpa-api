6432aebaaf35ef26dfd94cf2caaf29e286546d88
Gustavo Cotta <gmcotta34@gmail.com>
2020-10-15 15:37:52 -0300

    Creating institution without images

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,42 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+
+import Institution from './models/Institution';
 
 import './database/connection';
 
 const app = express();
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'NLW' });
+app.post('/institutions', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    retirement_or_center,
+    phone,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const institutionRepository = getRepository(Institution);
+
+  const institution = institutionRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    retirement_or_center,
+    phone,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+  await institutionRepository.save(institution);
+
+  return response.json(institution);
 });
 
 app.listen(3333);

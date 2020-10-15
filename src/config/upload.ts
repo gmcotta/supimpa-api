0ddb2c1f9105f831e5a2a6ae6bcffa3e9b4a7941
Gustavo Cotta <gmcotta34@gmail.com>
2020-10-15 17:20:58 -0300

    Configuring multer

---
+++ src/config/upload.ts
@@ -0,0 +1,15 @@
+import multer from 'multer';
+import path from 'path';
+import crypto from 'crypto';
+
+export default {
+  storage: multer.diskStorage({
+    destination: path.join(__dirname, '..', '..', 'uploads'),
+    filename: (request, file, cb) => {
+      crypto.randomBytes(16, (err, res) => {
+        if (err) return cb(err, '');
+        return cb(null, `${res.toString('hex')} - ${file.originalname}`);
+      });
+    },
+  }),
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from './config/upload';
 import institutionsController from './controllers/InstitutionsController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/institutions', institutionsController.index);
 routes.get('/institutions/:id', institutionsController.show);
-routes.post('/institutions', institutionsController.create);
+routes.post(
+  '/institutions',
+  upload.array('images'),
+  institutionsController.create,
+);
 
 export default routes;
